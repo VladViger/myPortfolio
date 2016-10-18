b38081b2cac01dc62211cb19504e9b7866943996
VladViger <vlad.v-mail@yandex.ru>
2016-10-18 19:02:35 +0300

    complate index and links pages

--- js/main_script.js
+++ js/main_script.js
@@ -2,64 +2,64 @@ var mainBlock = document.querySelector('.main-block');
 
 
 function getCoords(elem) {
-  var box = elem.getBoundingClientRect();
+	var box = elem.getBoundingClientRect();
 
-  return {
-    top: box.top + pageYOffset,
-    left: box.left + pageXOffset
-  };
+	return {
+		top: box.top + pageYOffset,
+		left: box.left + pageXOffset
+	};
 }
 
 
 function mixing(target) {
-    var cutPoint = target.nextElementSibling;
-    var oldReview = mainBlock.firstElementChild;
+	var cutPoint = target.nextElementSibling;
+	var oldReview = mainBlock.firstElementChild;
 
-    mainBlock.insertBefore(oldReview, cutPoint);
-    oldReview.className = 'thumb-bl';
+	mainBlock.insertBefore(oldReview, cutPoint);
+	oldReview.className = 'thumb-bl';
 
-    mainBlock.insertBefore(target, mainBlock.firstElementChild);
-    target.className = 'review';
+	mainBlock.insertBefore(target, mainBlock.firstElementChild);
+	target.className = 'review';
 }
 
 
 function mixingAnim(target) {
-    var cutPoint = target.nextElementSibling;
-    var oldReview = mainBlock.firstElementChild;
-    var fantomImg = oldReview.querySelector('.thumb-img');
-    var fantomParent = fantomImg.parentNode;
+	var cutPoint = target.nextElementSibling;
+	var oldReview = mainBlock.firstElementChild;
+	var fantomImg = oldReview.querySelector('.thumb-img');
+	var fantomParent = fantomImg.parentNode;
 
-    var trackCoords = {};
-    trackCoords.startPoint = getCoords(fantomImg);
-    trackCoords.endPoint = getCoords(target);
+	var trackCoords = {};
+	trackCoords.startPoint = getCoords(fantomImg);
+	trackCoords.endPoint = getCoords(target);
 
 // move fantomImg
-    document.body.appendChild(fantomImg);
+	document.body.appendChild(fantomImg);
 
-    fantomImg.style.right = fantomImg.style.bottom = 'auto';
-    fantomImg.style.top = trackCoords.startPoint.top + 'px';
-    fantomImg.style.left = trackCoords.startPoint.left + 'px';
+	fantomImg.style.right = fantomImg.style.bottom = 'auto';
+	fantomImg.style.top = trackCoords.startPoint.top + 'px';
+	fantomImg.style.left = trackCoords.startPoint.left + 'px';
 
-    setTimeout(function(){
-        fantomImg.style.top = trackCoords.endPoint.top + 'px';
-        fantomImg.style.left = trackCoords.endPoint.left + 'px';
-    }, 20);
+	setTimeout(function(){
+		fantomImg.style.top = trackCoords.endPoint.top + 'px';
+		fantomImg.style.left = trackCoords.endPoint.left + 'px';
+	}, 20);
 
 // clear fantomImg
-    setTimeout(function(){
-        fantomImg.style.cssText = '';
-        fantomParent.appendChild(fantomImg);
-    }, 900);
+	setTimeout(function(){
+		fantomImg.style.cssText = '';
+		fantomParent.appendChild(fantomImg);
+	}, 900);
 
 //cut old .review and insert
-    mainBlock.insertBefore(oldReview, cutPoint);
-    oldReview.className = 'thumb-bl';
+	mainBlock.insertBefore(oldReview, cutPoint);
+	oldReview.className = 'thumb-bl';
 
 //new .review
-    mainBlock.insertBefore(target, mainBlock.firstElementChild);
-    target.className = 'review marg-shift';
+	mainBlock.insertBefore(target, mainBlock.firstElementChild);
+	target.className = 'review marg-shift';
 
-    setTimeout(function(){ target.className = 'review' }, 20);
+	setTimeout(function(){ target.className = 'review' }, 20);
 }
 
 
@@ -68,16 +68,16 @@ var windWidth;
 
 
 mainBlock.onclick = function(event) {
-    var target = event.target;
-    if (target.className != 'thumb-bl') return;
+	var target = event.target;
+	if (target.className != 'thumb-bl') return;
 
-    windWidth = document.documentElement.clientWidth;
+	windWidth = document.documentElement.clientWidth;
 
-    (windWidth < 620 || sUsrAg.indexOf('MSIE 9.0') > -1) ? mixing(target) : mixingAnim(target);
+	(windWidth < 620 || sUsrAg.indexOf('MSIE 9.0') > -1) ? mixing(target) : mixingAnim(target);
 }
 
 
 /* fix background short img */
 if (document.body.scrollHeight > 1200) {
-    document.body.style.backgroundAttachment = 'fixed';
+	document.body.style.backgroundAttachment = 'fixed';
 }
